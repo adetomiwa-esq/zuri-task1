4b65cc59e0e70413ddf77c166c9bd14ec45f3065
Akinola Kehinde <khennyakin@gmail.com>
2022-10-29 14:33:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Profile from './components/Profile';
+import Links from './components/Links';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Links />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
